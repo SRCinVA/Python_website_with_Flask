c4b1b28822df2a77455b8bf5a475586b1af09c08
SRCinVA <scitarella@hotmail.com>
2022-06-03 22:14:47 -0400

    Numerous updates

--- Flask_web_app/website/auth.py
+++ Flask_web_app/website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 # we're going to define that our file is a blueprint of this application
 auth = Blueprint('auth', __name__)  # convention for how to name this
@@ -6,7 +6,7 @@ auth = Blueprint('auth', __name__)  # convention for how to name this
 
 @auth.route('/login')
 def login():
-    return "<p>Login</p>"
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
@@ -14,4 +14,4 @@ def logout():
 
 @auth.route('/sign-up')
 def sign_up():
-    return "<p>Sign Up</p>"
+    return render_template("sign_up.html")
