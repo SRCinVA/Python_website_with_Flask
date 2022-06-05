086f8b14300919a634075e82fc18b15d7c90fed8
SRCinVA <scitarella@hotmail.com>
2022-06-04 20:42:36 -0400

    More functionality with Jinja

--- Flask_web_app/website/auth.py
+++ Flask_web_app/website/auth.py
@@ -1,3 +1,4 @@
+from xmlrpc.client import Boolean
 from flask import Blueprint, render_template
 
 # we're going to define that our file is a blueprint of this application
@@ -6,8 +7,8 @@ auth = Blueprint('auth', __name__)  # convention for how to name this
 
 @auth.route('/login')
 def login():
-    return render_template("login.html")
-
+    return render_template("login.html", boolean=True) # interestingly, now we can access the text variable from the template
+                                                        # in other words, this is how you pass values to your templates
 @auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
