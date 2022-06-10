ac35f7c936483b9162a90c1418bf286520c94aea
SRCinVA <scitarella@hotmail.com>
2022-06-09 20:02:50 -0400

    More python for sign-up

--- Flask_web_app/website/auth.py
+++ Flask_web_app/website/auth.py
@@ -8,8 +8,8 @@ auth = Blueprint('auth', __name__)  # convention for how to name this
 
 @auth.route('/login', methods = ['GET', 'POST'])  
 def login():  # it's key for these methods to be able to accept requests (the second parameter, in this case)
-    data = request.form
-    print(data) # this is a way to test if the form attribute (the info you fill in) is passed (happily, it is).
+    # data = request.form
+    # print(data) # this is a way to test if the form attribute (the info you fill in) is passed (happily, it is).
     return render_template("login.html", boolean=True) # interestingly, now we can access the text variable from the template
                                                         # in other words, this is how you pass values to your templates
 @auth.route('/logout')
@@ -18,4 +18,23 @@ def logout():
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == "POST":
+        email = request.form.get('email') # as in, 'get' the email from the form (we still want to post to the backend).
+        firstName = request.form.get('firstName') 
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        # to validate the data before creating an account:
+        if len(email) < 4:
+            pass
+        elif len (firstName) < 2:
+            pass
+        elif password1 != password2:
+            pass
+        elif len(password1) < 7:
+            pass
+        else:
+            # add user to DB
+
+
     return render_template("sign_up.html")
