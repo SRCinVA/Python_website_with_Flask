07f7c22ad92003c7dc3065c460809720ccf9e79e
SRCinVA <scitarella@hotmail.com>
2022-06-13 21:18:43 -0400

    Started on DB

--- Flask_web_app/website/__init__.py
+++ Flask_web_app/website/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy() # this initializes the DB as an object
+DB_NAME = "database.db"  # (obviously) this is the name.
 
 def create_app(): # this initializes the app
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'MySecretKey'  # for encrypting session data, etc.
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
 
     # here's where we register our Blueprints, to tell flask what's going on.
     # that means we need to start to import them ...
