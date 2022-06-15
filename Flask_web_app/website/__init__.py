086562c7b5d07c1e97398823fa679959a57f636b
SRCinVA <scitarella@hotmail.com>
2022-06-14 21:29:11 -0400

    Can't get flask_login to work

--- Flask_web_app/website/__init__.py
+++ Flask_web_app/website/__init__.py
@@ -7,10 +7,13 @@ DB_NAME = "database.db"  # (obviously) this is the name.
 def create_app(): # this initializes the app
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'MySecretKey'  # for encrypting session data, etc.
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}' # this will store the DB in the website folder
 
     # here's where we register our Blueprints, to tell flask what's going on.
     # that means we need to start to import them ...
+
+    # here, we're going to initialize our database by giving it our Flask app.
+    db.init_app(app)  # we're telling db (up top) that this is the app we'll be using it in.
 
     from .views import views
     from .auth import auth
---
+++ Flask_web_app/website/models.py
@@ -0,0 +1,5 @@
+from . import db  # here the dot refers to the overall package. Form outside the directory it would be 'website'
+from flask_login import LoginManager
+
+
+
